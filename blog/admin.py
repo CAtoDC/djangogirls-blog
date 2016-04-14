a13546242412518c48063df17387a2add6bf624a
CAtoDC <mark.cuevas@gmail.com>
2016-04-14 10:05:24 -0400

    Added fields to Post and Comment in Admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+class PostModelAdmin(admin.ModelAdmin):
+    '''Adds fields to the post portion in the admin view'''
+    list_display = ["title", "published_date", "created_date"]
+    list_display_links = ["published_date"]
+    list_editable = ["title"]
+    list_filter = ["published_date"]
+    search_fields = ["title", "text"]
+    class Meta:
+        model = Post
+
+
+class CommentModelAdmin(admin.ModelAdmin):
+    '''Adds fields to the comment portion in the admin view'''
+    list_display = ["post", "created_date"]
+    list_display_links = ["created_date"]
+    search_fields = ["text"]
+    class Meta:
+        model = Comment
+
 # make these available in the Admin panel
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Comment, CommentModelAdmin)
---
+++ blog/crud.md
@@ -0,0 +1,7 @@
+CREATE -- POST -- Make new
+RETRIEVE -- GET -- List / Search (Does not require permissions)
+UPDATE -- PUT -- Edit
+DELETE -- DELETE -- Delete
+
+
+Views.py allows us to use each of these methods.
